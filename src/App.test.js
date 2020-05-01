9cbc458553f781184bb24a6e2e035001df9e0b72
Moritz Brandstätter <arriggon@gmail.com>
2020-05-01 16:18:22 +0200

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+import { MemoryRouter } from "react-router-dom";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
+test("expect to display correct route", () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={["/"]}>
+      <App />
+    </MemoryRouter>
+  );
   const headerText = getByText(/HATS/i);
   expect(headerText).toBeInTheDocument();
 });
