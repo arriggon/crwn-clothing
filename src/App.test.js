d2878a8d259538f4753d39d6359834409a041db1
Moritz Brandstätter <arriggon@gmail.com>
2020-05-01 16:09:49 +0200

    update test and utilize jest and enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from "react";
-import { render } from "@testing-library/react";
 import App from "./App";
+import { MemoryRouter } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
+import { mount } from "enzyme";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const headerText = getByText(/HATS/i);
-  expect(headerText).toBeInTheDocument();
+Enzyme.configure({ adapter: new Adapter() });
+
+test("check valid routing", () => {
+  const wrapper = mount(
+    <MemoryRouter initialEntries={["/"]}>
+      <App />
+    </MemoryRouter>
+  );
+
+  expect(wrapper.find(HomePage)).toHaveLength(1);
 });
