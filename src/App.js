903e07b7fe6acebe5f39601a5244a9464f21f524
Moritz Brandstätter <arriggon@gmail.com>
2020-05-02 15:29:00 +0200

    update menu-item to use router, add 404-page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,34 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { Route, Switch } from "react-router-dom";
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+const Page404 = () => (
+  <div>
+    <h1>404 Page not found</h1>
+    <p>
+      We are sorry, but we couldn't find the page you were trying to navigate
+      to!
+    </p>
+    <p>Please check your URL and try again!</p>
+    <p>
+      If this issue keeps happening, please{" "}
+      <a href="mailto:contact@moritz-b.com">contact us</a>!
+    </p>
+  </div>
+);
+
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={HatsPage} />
+        <Route component={Page404} />
       </Switch>
     </div>
   );
