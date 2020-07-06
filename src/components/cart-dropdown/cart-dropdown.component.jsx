ca90b4b1613fa60b6560a1804c39c89498341344
Moritz Brandstaetter <contact@moritz-b.com>
2020-07-06 05:42:07 +0200

    add cart-item display

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,12 +2,29 @@ import React from "react";
 
 import "./cart-dropdown.component.scss";
 import CustomButton from "../custom-button/custom-button.component";
+import CartItem from "../cart-item/cart-item.component";
+import { connect } from "react-redux";
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-items"></div>
+    <div className="cart-items">
+      {cartItems.map((cartItem) => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
+    SUM: {"$" + getSum(cartItems)}
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+const getSum = (cartItems) => {
+  let sum = 0;
+  cartItems.map((cartItem) => (sum += cartItem.price * cartItem.quantity));
+  return sum;
+};
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
